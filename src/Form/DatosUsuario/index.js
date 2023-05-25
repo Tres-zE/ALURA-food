207a0ad062fd02ce8a46dd3bffcbb107bf9fbb3f
Migue-San <lx15726@innovaccion.mx>
2023-05-25 16:51:58 -0600

    subiendO CONTRASEÑA Y USUARIO

--- src/Form/DatosUsuario/index.js
+++ src/Form/DatosUsuario/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarEmail, validarPassword } from "./Validaciones.js"
 
-class DatosUsuario extends React.Component {
-  render() {
+const DatosUsuario = () => {
+  const [email, setEmail] = useState({value: "", valid: true})
+  const [password, setPassword] = useState({value: "", valid: true})
+  
+  
     return (
       <Box
         component="form"
@@ -12,8 +16,12 @@ class DatosUsuario extends React.Component {
           alignItems: "center",
           justifyContent: "center",
           flexDirection: "column",
+        }} 
+        onSubmit={(e) =>{
+          e.preventDefault()
+          console.log(email,password)
         }}
-      >
+        >
         <TextField
           label="Correo electrónico"
           variant="outlined"
@@ -22,6 +30,12 @@ class DatosUsuario extends React.Component {
           type="email"
           error={false}
           helperText={false && "Ingresa un correo electrónico válido"}
+          value={email.value}
+          onChange={(input) => {
+            const email = input.target.value
+            const valido = validarEmail(email)
+            setEmail( {value: email, valid:valido} )}
+          } 
         />
         <TextField
           label="Contraseña"
@@ -29,13 +43,19 @@ class DatosUsuario extends React.Component {
           fullWidth
           margin="dense"
           type="password"
+          value={password.value}
+          onChange={(input) => {
+            const password = input.target.value
+            
+            setPassword( {value: password, valid:validarPassword(password)} )}
+          } 
         />
         <Button variant="contained" type="submit">
           Siguiente
         </Button>
       </Box>
     );
-  }
+  
 }
 
 export default DatosUsuario;
